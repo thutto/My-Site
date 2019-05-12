72a613fb15f6cac2b8fae42b4089c7b5cde761ed
Travis Hutto <t.hutto4@gmail.com>
2019-05-12 13:20:02 -0400

    Feature: Fix Build Command and Make Build for EC2

---
+++ build/precache-manifest.4e2e8255297f5bafde3e315217d5ba36.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "08af86157d69d01ac524",
+    "url": "/static/css/main.5173d827.chunk.css"
+  },
+  {
+    "revision": "08af86157d69d01ac524",
+    "url": "/static/js/main.08af8615.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1c3053d5fdf54e557bc7",
+    "url": "/static/js/2.1c3053d5.chunk.js"
+  },
+  {
+    "revision": "2e4461aee1fe690b8c4ff61934f4ea51",
+    "url": "/static/media/Brittany_Avatar.2e4461ae.jpg"
+  },
+  {
+    "revision": "a48b74e0158d7e748f78ff508d33cbcb",
+    "url": "/static/media/Mark_Avatar.a48b74e0.jpg"
+  },
+  {
+    "revision": "a9b13a624192183de79ac2b568a53f12",
+    "url": "/static/media/Travis_Avatar.a9b13a62.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "12e55b4be5002f75b971d88af1a0f3d1",
+    "url": "/index.html"
+  }
+];
