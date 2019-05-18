ba45760fd3c18581eb578ec8f7fb4a8a19375aaa
Travis Hutto <t.hutto4@gmail.com>
2019-05-18 19:57:38 -0400

    Build: Latest Prod Build to add support for profile page and blog link

---
+++ build/precache-manifest.195a5fe2942268b22a184fe9c34ff910.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "d3a6e1423b6cb00fdac4",
+    "url": "/static/css/main.5173d827.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "52274c5c56e3541e2aba",
+    "url": "/static/js/2.52274c5c.chunk.js"
+  },
+  {
+    "revision": "2e4461aee1fe690b8c4ff61934f4ea51",
+    "url": "/static/media/Brittany_Avatar.2e4461ae.jpg"
+  },
+  {
+    "revision": "a48b74e0158d7e748f78ff508d33cbcb",
+    "url": "/static/media/Mark_Avatar.a48b74e0.jpg"
+  },
+  {
+    "revision": "a9b13a624192183de79ac2b568a53f12",
+    "url": "/static/media/Travis_Avatar.a9b13a62.jpg"
+  },
+  {
+    "revision": "d3a6e1423b6cb00fdac4",
+    "url": "/static/js/main.d3a6e142.chunk.js"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "ad8ac34ae5d78f42241adf2352e57512",
+    "url": "/index.html"
+  }
+];
